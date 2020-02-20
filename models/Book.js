6959dbd7a5fc514cd319027b98f8c823fe4b9524
Lialichev <lyalichevv98@gmail.com>
2020-02-20 22:31:10 +0200

    create follow / unfollow points

--- models/Book.js
+++ models/Book.js
@@ -4,6 +4,7 @@ const schema = new Schema({
   name: { type: String, required: true },
   year: { type: Number, required: true },
   followers: [{ type: Types.ObjectId, ref: 'User' }],
+  creator: { type: Types.ObjectId, ref: 'User' },
 });
 
 module.exports = model('Book', schema);
--- routes/book.routes.js
+++ routes/book.routes.js
@@ -3,6 +3,7 @@ const Book = require('../models/Book');
 const auth = require('../middleware/auth.middleware');
 const router = Router();
 
+// /api/book/create
 router.post('/create', auth, async (req, res) => {
   try {
     const { name, year } = req.body;
@@ -13,10 +14,11 @@ router.post('/create', auth, async (req, res) => {
       return res.json({ name: existing });
     }
 
-    console.log(req.user.user_id);
-
     const newBook = new Book({
-      name, year, followers: req.user.user_id
+      name,
+      year,
+      followers: req.user.user_id,
+      creator: req.user.user_id,
     });
 
     await newBook.save();
@@ -24,6 +26,62 @@ router.post('/create', auth, async (req, res) => {
     res.status(201).json({ newBook });
   } catch (e) {
     res.status(500).json({ message: 'Error book create' });
+  }
+});
+
+// /api/book/follow
+router.post('/follow', auth, async (req, res) => {
+  try {
+    const { id } = req.body;
+
+    const book = await Book.findById(id);
+
+    if (!book) {
+      return res.status(404).json({ book });
+    }
+
+    if (book.followers.includes(req.user.user_id)) {
+      return res.json({ book });
+    }
+
+    book.followers.push(req.user.user_id);
+
+    await book.save();
+
+    res.status(201).json({ book });
+  } catch (e) {
+    res.status(500).json({ message: 'Error follow' });
+  }
+});
+
+// /api/book/unfollow
+router.post('/unfollow', auth, async (req, res) => {
+  try {
+    const { id } = req.body;
+
+    const book = await Book.findById(id);
+
+    if (!book) {
+      return res.status(404).json({ book });
+    }
+
+    if (req.user.user_id === book.creator.toString()) {
+      return res.status(403).json({ message: 'User is creator' });
+    }
+
+    if (!book.followers.includes(req.user.user_id)) {
+      return res.json({ book });
+    }
+
+    const index = book.followers.indexOf(req.user.user_id);
+
+    book.followers.splice(index, 1);
+
+    await book.save();
+
+    res.status(201).json({ book });
+  } catch (e) {
+    res.status(500).json({ message: 'Error unfollow' });
   }
 });
 
