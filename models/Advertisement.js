0321db75000b1ac54993641ada57cb27c9acda17
Lialichev <lyalichevv98@gmail.com>
2020-07-24 23:54:47 +0300

    feature: add Advertisement model

--- models/Advertisement.js
+++ models/Advertisement.js
@@ -2,12 +2,39 @@ const { Schema, model, Types } = require('mongoose');
 
 const schema = new Schema(
   {
-    category_id: { type: Types.ObjectId, required: true, ref: 'Category' },
-    brand_id: { type: Types.ObjectId, required: true, ref: 'Brand' },
-    model_id: { type: Types.ObjectId, required: true, ref: 'Model' },
-    region_id: { type: Types.ObjectId, required: true, ref: 'Region' },
-    year: { type: Number, required: true, min: 4, max: 4 },
-    price: { type: Number, required: true },
+    photos: [ { type: String } ],
+    general: {
+      category_id: { type: Types.ObjectId, required: true, ref: 'Category' },
+      brand_id: { type: Types.ObjectId, required: true, ref: 'Brand' },
+      model_id: { type: Types.ObjectId, required: true, ref: 'Model' },
+      year: { type: Number, required: true, min: 4, max: 4 },
+      modification: { type: String },
+      body_type_id: { type: Types.ObjectId, required: true, ref: 'BodyType' },
+      mileage: { type: Number, required: true, min: 1, max: 99999 },
+      region_id: { type: Types.ObjectId, required: true, ref: 'Region' },
+      city_id: { type: Types.ObjectId, required: true, ref: 'City' },
+      VIN: { type: String },
+    },
+    price: {
+      value: { type: Number, required: true },
+      currency_id: { type: Types.ObjectId, required: true, ref: 'Currency' },
+      auctions: { type: Boolean, default: false },
+      exchangesAutoAllowed: { type: Boolean, default: false }
+    },
+    description: { type: String, maxLength: 2000 },
+    additional_characteristics: {
+      color_id: { type: Types.ObjectId, ref: 'Color' },
+      technical_condition_id: { type: Types.ObjectId, ref: 'TechnicalCondition' },
+      country_import_id: { type: Types.ObjectId, ref: 'Country' },
+    },
+    contacts: {
+      phone: { type: String, required: true }
+    },
+    agreement: { type: Boolean, required: true },
+    published_date: { type: Date, required: true, default: new Date() },
+    updated_date: { type: Date, required: true, default: new Date() },
+    owner_id: { type: Types.ObjectId, required: true, ref: 'User' },
+    status_id: { type: Types.ObjectId, required: true, ref: 'Status' }
   },
   {
     versionKey: false
--- models/User.js
+++ models/User.js
@@ -1,9 +1,13 @@
-const { Schema, model, Types } = require('mongoose');
+const { Schema, model } = require('mongoose');
 
-const schema = new Schema({
-  email: { type: String, required: true, unique: true },
-  password: { type: String, required: true },
-  // books: [{ type: Types.ObjectId, ref: 'Book' }],
-});
+const schema = new Schema(
+  {
+    email: { type: String, required: true, unique: true },
+    password: { type: String, required: true },
+  },
+  {
+    versionKey: false
+  }
+);
 
 module.exports = model('User', schema);
